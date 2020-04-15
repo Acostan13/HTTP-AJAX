7f9a3f754083de6857d9c5638cf0313b4608a546
Acostan13 <acostan@fau.edu>
2020-04-15 12:06:22 -0400

    Stlying the Active Route

--- src/components/Post/Post.js
+++ src/components/Post/Post.js
@@ -6,7 +6,7 @@ const post = (props) => (
     <article onClick={props.clicked} className="Post">
         <h1>{props.title}</h1>
         <div className="Info">
-<div className="Author">{props.author}</div>
+            <div className="Author">{props.author}</div>
         </div>
     </article>
 );
