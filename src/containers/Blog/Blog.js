033e9558e70cd596ad7e10d1bbd274e50c831306
Acostan13 <acostan@fau.edu>
2020-05-11 10:01:16 -0400

    Setting Up Links

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -14,10 +14,10 @@ class Blog extends Component {
         error: false
     }
 
-    componentDidMount () {
+    componentDidMount() {
         axios.get('/posts')
             .then(response => {
-                const posts = response.data.slice(0,4)
+                const posts = response.data.slice(0, 4)
                 const updatedPosts = posts.map(post => {
                     return {
                         ...post,
@@ -29,7 +29,7 @@ class Blog extends Component {
                     // console.log(response)
                 })
             })
-            .catch(error =>{
+            .catch(error => {
                 //console.log(error)
                 this.setState({
                     error: true
@@ -37,19 +37,19 @@ class Blog extends Component {
             })
     }
 
-    postSelectedHandler (id) {
+    postSelectedHandler(id) {
         this.setState({
             selectedPostId: id
         })
     }
 
-    render () {
-        let posts = <p style={{textAlign: 'center'}}>Something went wrong!</p>
-        if(!this.state.error){
+    render() {
+        let posts = <p style={{ textAlign: 'center' }}>Something went wrong!</p>
+        if (!this.state.error) {
             posts = this.state.posts.map(post => {
-                return <Post 
-                    author={post.author} 
-                    key={post.id} 
+                return <Post
+                    author={post.author}
+                    key={post.id}
                     clicked={() => this.postSelectedHandler(post.id)}
                     title={post.title} />
             })
@@ -57,9 +57,17 @@ class Blog extends Component {
 
 
         return (
-            <div>
+            <div className="Blog">
+                <header>
+                    <nav>
+                        <ul>
+                            <li><a href='/'>Home</a></li>
+                            <li><a href='/new-post'>New Post</a></li>
+                        </ul>
+                    </nav>
+                </header>
                 <section className="Posts">
-                {posts}
+                    {posts}
                 </section>
                 <section>
                     <FullPost id={this.state.selectedPostId} />
