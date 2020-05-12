8334195ff733e20a397b50420a22453ccefd65b0
Acostan13 <acostan@fau.edu>
2020-05-12 11:27:09 -0400

    Rendering Components for Routes

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,14 +1,13 @@
 import React, { Component } from 'react';
 import {Route} from 'react-router-dom'
 
-//import Posts from './Posts/Posts'
+import Posts from './Posts/Posts'
 import './Blog.css';
 
 class Blog extends Component {
 
 
     render() {
-
         return (
             <div className="Blog">
                 <header>
@@ -19,8 +18,9 @@ class Blog extends Component {
                         </ul>
                     </nav>
                 </header>
-                <Route path="/" exact render={() => <h1>Home</h1>}/>
-                <Route path="/" render={() => <h1>Home 2</h1>}/>
+                {/* <Route path="/" exact render={() => <h1>Home</h1>}/>
+                <Route path="/" render={() => <h1>Home 2</h1>}/> */}
+                <Route path="/" exact component={Posts}/>
             </div>
         );
     }
